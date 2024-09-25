f0df4c8b1be6e6ad9e5ab350f3ec2ff6fb5cf4cd
ShIreen66 <066shiree@gmail.com>
2024-09-25 09:34:25 +0530

    first commit

---
+++ New folder/script.js
@@ -0,0 +1,21 @@
+// functions
+
+// function declearation/initialization
+// function funcname() {
+//     console.log("Function Body");
+// }
+
+// // funciton calling
+// funcname();
+
+// 1. by default function returns undefined
+// 2. return must be the last line of the function
+// 3. only single entity must be returned by the funciton
+// 4. function call is replaced by the return value
+// 5. code written after return in the function will not be reachable
+
+
+// // funciton calling
+// funcname();
+// let x = funcname();
+// console.log(x);
